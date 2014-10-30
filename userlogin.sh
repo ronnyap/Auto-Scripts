daf70531a253815a6da453d5cf9f4234a04756cb
IlhamArrouf <ilham.arrouf@gmail.com>
2014-10-30 17:15:30 +0700

    Update userlogin.sh

--- userlogin.sh
+++ userlogin.sh
@@ -37,9 +37,4 @@ do
         fi
 done
 
-echo "Checking PPTP Login"
-echo "-------------------"
-last | grep ppp | grep still
-
-echo "-----------------------------------------------"
 echo "Script Modified by IlhamGanteng";
