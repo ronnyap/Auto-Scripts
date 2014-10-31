50942a5782e1a910b892b059282849a75eec3746
IlhamArrouf <ilham.arrouf@gmail.com>
2014-10-31 15:41:44 +0700

    Update autoscript.sh

--- autoscript.sh
+++ autoscript.sh
@@ -7,6 +7,7 @@ wget -O /usr/local/bin/user-login "https://raw.githubusercontent.com/IlhamArrouf
 wget -O /usr/local/bin/speedtest "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/speedtest.sh.x"
 wget -O /usr/local/bin/user-expired "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/user-expired.sh.x"
 wget -O /usr/local/bin/user-limit "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/user-limit.sh.x"
+wget -O /usr/local/bin/expire "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/expire.sh.x"
 echo "0 0 * * * root /usr/local/bin/user-expired" > /etc/cron.d/user-expired
 chmod +x /usr/local/bin/ram
 chmod +x /usr/local/bin/trial
@@ -15,3 +16,4 @@ chmod +x /usr/local/bin/user-login
 chmod +x /usr/local/bin/speedtest
 chmod +x /usr/local/bin/user-expired
 chmod +x /usr/local/bin/user-limit
+chmod +x /usr/local/bin/expire
