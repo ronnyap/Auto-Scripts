88b9169c68e3a14728930d18c873209c20c3b6ea
IlhamArrouf <ilham.arrouf@gmail.com>
2014-10-31 16:02:59 +0700

    Update autoscript.sh

--- autoscript.sh
+++ autoscript.sh
@@ -8,7 +8,10 @@ wget -O /usr/local/bin/speedtest "https://raw.githubusercontent.com/IlhamArrouf/
 wget -O /usr/local/bin/user-expired "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/user-expired.sh.x"
 wget -O /usr/local/bin/user-limit "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/user-limit.sh.x"
 wget -O /usr/local/bin/expire "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/expire.sh.x"
+wget -O /usr/local/bin/autokill "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/autokill.sh.x"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/banner"
 echo "0 0 * * * root /usr/local/bin/user-expired" > /etc/cron.d/user-expired
+sed -i '$ i\screen -AmdS check /usr/local/bin/autokill' /etc/rc.local
 chmod +x /usr/local/bin/ram
 chmod +x /usr/local/bin/trial
 chmod +x /usr/local/bin/alluser
@@ -17,3 +20,4 @@ chmod +x /usr/local/bin/speedtest
 chmod +x /usr/local/bin/user-expired
 chmod +x /usr/local/bin/user-limit
 chmod +x /usr/local/bin/expire
+chmod +x /usr/local/bin/autokill
