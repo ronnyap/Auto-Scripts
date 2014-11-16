2a6047c9a518f292e3a809cadfa819ba20e058fe
IlhamArrouf <ilham.arrouf@gmail.com>
2014-11-16 17:50:14 +0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -73,7 +73,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>Modified by IlhamGanteng</pre>" > /home/vps/public_html/index.html
+echo "<pre>Setup by IlhamGanteng</pre>" > /home/vps/public_html/index.html
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/IlhamArrouf/IlhamGanteng/master/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
